0cf62654f09961aa2a35c741b48c22c99a58dd68
stephen haunts <stephen.haunts@outlook.com>
2018-01-02 20:42:03 +0000

    Add initial project

---
+++ Complete/SecureCodingWorkshop/RandomNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RandomNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
