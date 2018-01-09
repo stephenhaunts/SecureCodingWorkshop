1c33db0c76ce8283a67a2b1317c2b92873668a3d
stephen haunts <stephen.haunts@outlook.com>
2018-01-09 15:09:43 +0000

    Add securestring and key vault code

---
+++ Complete/SecureCodingWorkshop/KeyVault/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SecureCodingWorkshop.KeyVault
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
