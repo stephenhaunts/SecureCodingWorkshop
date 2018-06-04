4bbba9a2693c09f03328c8f83375c855a0be41f6
stephen haunts <stephen.haunts@outlook.com>
2018-06-04 15:03:58 +0100

    Add Key Vault examples to work-along version of workshop.

---
+++ Workshop/SecureCodingWorkshop/KV_Secrets/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureKeyVault.Secrets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KeyVault().GetAwaiter().GetResult();
+        }
+
+        public static async Task KeyVault()
+        {
+            IKeyVault vault = new KeyVault();
+
+            const string MY_SECRET = "StephenHauntsSecret";
+
+            var secretId = await vault.SetSecretAsync(MY_SECRET, "Mary had a little lamb.");
+            Console.WriteLine("Secret Written");
+
+            string secret = await vault.GetSecretAsync(MY_SECRET);
+            Console.WriteLine("Secret Retrieved : " + secret);
+        }
+    }
+}
