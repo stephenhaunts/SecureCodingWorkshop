b8c2130fa7550170f501c4349d080c84336868e5
Stephen Haunts <stephen.haunts@outlook.com>
2020-01-06 15:50:23 +0000

    Add AES GCM demo code to workshop solution

---
+++ Workshop/SecureCodingWorkshop/AES/AesGCMEncryption.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SecureCodingWorkshop.AES
+{
+    public class AesGCMEncryption
+    {
+        public byte[] GenerateRandomNumber(int length)
+        {
+            using (var randomNumberGenerator = new RNGCryptoServiceProvider())
+            {
+                var randomNumber = new byte[length];
+                randomNumberGenerator.GetBytes(randomNumber);
+
+                return randomNumber;
+            }
+        }
+
+        public (byte[], byte[]) Encrypt(byte[] dataToEncrypt, byte[] key, byte[] nonce, byte[] associatedData)
+        {
+            // these will be filled during the encryption
+            byte[] tag = new byte[16];
+            byte[] ciphertext = new byte[dataToEncrypt.Length];
+
+            using (AesGcm aesGcm = new AesGcm(key))
+            {
+                aesGcm.Encrypt(nonce, dataToEncrypt, ciphertext, tag, associatedData);
+            }
+
+            return (ciphertext, tag);
+        }
+
+        public byte[] Decrypt(byte[] cipherText, byte[] key, byte[] nonce, byte[] tag, byte[] associatedData)
+        {
+            byte[] decryptedData = new byte[cipherText.Length];
+
+            using (AesGcm aesGcm = new AesGcm(key))
+            {
+                aesGcm.Decrypt(nonce, cipherText, tag, decryptedData, associatedData);
+            }
+
+            return decryptedData;
+        }    
+    }
+}
--- Workshop/SecureCodingWorkshop/AES/Program.cs
+++ Workshop/SecureCodingWorkshop/AES/Program.cs
@@ -7,10 +7,19 @@ namespace SecureCodingWorkshop.AES
     {
         static void Main(string[] args)
         {
+            // AES CBC
             // TODO : Generate a 32 byte key
             // TODO : Generate a 16 byte initialization vector
             // TODO : Encrypt some text, base64 the result and display to the console.
             // TODO : Decrypt the encrypted text, base64 the result and display to the console.
+
+
+            // AES GCM
+            // TODO : Generate a 32 byte key
+            // TODO : Generate a 12 byte initialization vector
+            // TODO : Encrypt some text, base64 the result and display to the console.
+            // TODO : Decrypt the encrypted text, base64 the result and display to the console.
+            // TODO : Don't forget to store the tag that comes back from the encryption as you will need it to decrypt.
         }
     }
 }
