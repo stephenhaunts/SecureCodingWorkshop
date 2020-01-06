87ee5af4382439a3cb0e848f1fa8ee60ea40b4fd
Stephen Haunts <stephen.haunts@outlook.com>
2020-01-06 16:09:49 +0000

    Add GCM examples to the follow along workshop code.

---
+++ Workshop/SecureCodingWorkshop/HybridWithIntegrityAndSignatureGCM/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SecureCodingWorkshop.HybridWithIntegrityAndSignature
+{
+    static class Program
+    {
+        static void Main()
+        {
+           
+        }
+    }
+}
