1c87a50d00606bfeea057033920e49ebdf007113
Stephen Haunts <stephen.haunts@outlook.com>
2020-01-08 14:45:52 +0000

    Add simpler secure string example.

--- Complete/SecureCodingWorkshop/SecureString/Program.cs
+++ Complete/SecureCodingWorkshop/SecureString/Program.cs
@@ -6,76 +6,44 @@ namespace SecureCodingWorkshop.String
 {
     static class Program
     {
+
+        // https://docs.microsoft.com/en-us/dotnet/api/system.security.securestring?view=netcore-3.1
         static void Main()
         {
-            var str = ToSecureString(new[] { '5', '1', '2', '5' });
+            // Instantiate the secure string.
+            SecureString securePwd = new SecureString();
+            ConsoleKeyInfo key;
 
-            char[] charArray = CharacterData(str);
-
-            string unsecure = ConvertToUnsecureString(str);
-        }
-
-
-        private static SecureString ToSecureString(char[] str)
-        {
-            var secureString = new SecureString();
-
-            // Append each character onto the SecureString
-            Array.ForEach(str, secureString.AppendChar);
-
-            return secureString;
-        }
-
-
-        private static char[] CharacterData(SecureString secureString)
-        {
-            char[] bytes;
-            var ptr = IntPtr.Zero;
-
-            try
+            Console.Write("Enter password: ");
+            do
             {
-                // Allocate a BSTR and copy contents of SecureString into it
-                ptr = Marshal.SecureStringToBSTR(secureString);
-                bytes = new char[secureString.Length];
+                key = Console.ReadKey(true);
 
-                // Copy data from unmanaged memory into a managed char array
-                Marshal.Copy(ptr, bytes, 0, secureString.Length);
-            }
-            finally
-            {
-                if (ptr != IntPtr.Zero)
+                // Ignore any key out of range.
+                if (((int)key.Key) >= 65 && ((int)key.Key <= 90))
                 {
-                    // Free unmanaged memory
-                    Marshal.ZeroFreeBSTR(ptr);
+                    // Append the character to the password.
+                    securePwd.AppendChar(key.KeyChar);
+                    Console.Write("*");
                 }
-            }
-
-            return bytes;
-        }
-
-        private static string ConvertToUnsecureString(SecureString securePassword)
-        {
-            if (securePassword == null)
-            {
-                throw new ArgumentNullException("securePassword");
-            }
-
-            var unmanagedString = IntPtr.Zero;
+                // Exit if Enter key is pressed.
+            } while (key.Key != ConsoleKey.Enter);
+            Console.WriteLine();
 
             try
             {
-                // Copy the contents of the SecureString to unmanaged memory
-                unmanagedString = Marshal.SecureStringToGlobalAllocUnicode(securePassword);
-
-                // Allocate a managed string and copy the contents of the unmanaged 
-                //string data into it
-                return Marshal.PtrToStringUni(unmanagedString);
+                //Process.Start("Notepad.exe", "MyUser", securePwd, "MYDOMAIN");
             }
+            //catch (Win32Exception e)
+            //{
+            //    Console.WriteLine(e.Message);
+            //}
             finally
             {
-                // Free the unmanaged string pointer
-                Marshal.ZeroFreeGlobalAllocUnicode(unmanagedString);
+                securePwd.Dispose();
             }
+
         }
+
     }
 }
