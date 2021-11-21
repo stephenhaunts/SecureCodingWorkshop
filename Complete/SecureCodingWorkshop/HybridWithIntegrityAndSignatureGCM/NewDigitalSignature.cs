a10be7b752a24e8061066018f2d6695189c11a5e
Stephen Haunts <stephen.haunts@outlook.com>
2021-11-21 14:55:48 +0000

    Update Hybrid Examples

---
+++ Complete/SecureCodingWorkshop/HybridWithIntegrityAndSignatureGCM/NewDigitalSignature.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Pluralsight.HybridWithIntegrityAndSignatureGCM
+{
+	public class NewDigitalSignature
+    {
+        private RSA rsa; 
+ 
+        public NewDigitalSignature()
+        {
+            rsa = RSA.Create(2048);
+        }
+
+
+        public byte[] SignData(byte[] dataToSign)
+        {
+            return (rsa.SignHash(dataToSign, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1));
+        }
+
+        public bool VerifySignature(byte[] signature, byte[] hashOfDataToSign)
+        {
+            return rsa.VerifyHash(hashOfDataToSign, signature, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);            
+        }
+
+        public byte[] ExportPrivateKey(int numberOfIterations, string password)
+        {
+            byte[] encryptedPrivateKey = new byte[2000];
+           
+            PbeParameters keyParams = new PbeParameters(PbeEncryptionAlgorithm.Aes256Cbc, HashAlgorithmName.SHA256, numberOfIterations);  
+            encryptedPrivateKey = rsa.ExportEncryptedPkcs8PrivateKey(Encoding.UTF8.GetBytes(password), keyParams);
+
+            return encryptedPrivateKey;
+        }
+
+        public void ImportEncryptedPrivateKey(byte[] encryptedKey, string password)
+        {
+            rsa.ImportEncryptedPkcs8PrivateKey(Encoding.UTF8.GetBytes(password), encryptedKey, out _);
+        }
+
+        public byte[] ExportPublicKey()
+        {
+            return rsa.ExportRSAPublicKey();
+        }
+
+        public void ImportPublicKey(byte[] publicKey)
+        {
+            rsa.ImportRSAPublicKey(publicKey, out _);
+        }
+    }
+}
---
+++ Complete/SecureCodingWorkshop/HybridWithIntegrityAndSignatureGCM/NewRSA.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Pluralsight.HybridWithIntegrityAndSignatureGCM
+{
+	public class NewRSA
+    {
+        private RSA rsa; 
+ 
+        public NewRSA()
+        {
+            rsa = RSA.Create(2048);
+        }
+
+        public byte[] Encrypt(string dataToEncrypt)
+        {
+            return rsa.Encrypt(Encoding.UTF8.GetBytes(dataToEncrypt), RSAEncryptionPadding.OaepSHA256);
+        }
+
+        public byte[] Encrypt(byte[] dataToEncrypt)
+        {
+            return rsa.Encrypt(dataToEncrypt, RSAEncryptionPadding.OaepSHA256);
+        }
+
+        public byte[] Decrypt(byte[] dataToDecrypt)
+        {
+            return rsa.Decrypt(dataToDecrypt, RSAEncryptionPadding.OaepSHA256);
+        }
+
+        public byte[] ExportPrivateKey(int numberOfIterations, string password)
+        {
+            byte[] encryptedPrivateKey = new byte[2000];
+           
+            PbeParameters keyParams = new PbeParameters(PbeEncryptionAlgorithm.Aes256Cbc, HashAlgorithmName.SHA256, numberOfIterations);  
+            encryptedPrivateKey = rsa.ExportEncryptedPkcs8PrivateKey(Encoding.UTF8.GetBytes(password), keyParams);
+
+            return encryptedPrivateKey;
+        }
+
+        public void ImportEncryptedPrivateKey(byte[] encryptedKey, string password)
+        {
+            rsa.ImportEncryptedPkcs8PrivateKey(Encoding.UTF8.GetBytes(password), encryptedKey, out _);
+        }
+
+        public byte[] ExportPublicKey()
+        {
+            return rsa.ExportRSAPublicKey();
+        }
+
+        public void ImportPublicKey(byte[] publicKey)
+        {
+            rsa.ImportRSAPublicKey(publicKey, out _);
+        }
+    }
+}
